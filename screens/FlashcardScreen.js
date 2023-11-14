41f08a3f2e1831dacc11ad814bc8a456e37b0f1a
reynashah <71095655+reynashah@users.noreply.github.com>
2023-11-14 14:33:28 -0600

    a
    a

---
+++ screens/FlashcardScreen.js
@@ -0,0 +1,16 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { signOut } from 'firebase/auth'
+import { auth } from '../config/firebase'
+
+export default function FlashcardScreen() {
+
+
+    
+  return (
+    <SafeAreaView className="flex-1 flex-row justify-center items-center">
+
+    </SafeAreaView>
+  )
+}
