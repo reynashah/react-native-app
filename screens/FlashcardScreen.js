64b988360add305dad76b3f22dba97fe23dbe35d
aidai <123458398+akmaidai@users.noreply.github.com>
2023-11-20 12:14:37 -0600

    new api used

--- screens/FlashcardScreen.js
+++ screens/FlashcardScreen.js
@@ -13,31 +13,47 @@ import queryString from 'query-string';
 const FlashcardScreen = ({ route }) => {
   const { selectedLanguage, selectedCategory } = route.params;
   const navigation = useNavigation();
+  const translateText = async (text) => {
+    const apiKey = '526bb5e251msh7aa7fa5103b1bffp155249jsnf817a86771f2';
 
-  const translateText = async (text, targetLanguage) => {
+    const translateSingleText = async (singleText) => {
+      const url = 'https://text-translator2.p.rapidapi.com/translate';
+
+      const formData = new FormData();
+      formData.append('source_language', 'en');
+      formData.append('target_language', selectedLanguage);
+      formData.append('text', singleText);
+
+      try {
+        const response = await axios.post(url, formData, {
+          headers: {
+            'content-type': 'multipart/form-data',
+            'X-RapidAPI-Key': apiKey,
+            'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com',
+          },
+        });
+
+        console.log('API Response:', response.data);
+
+        if (response.data && response.data.status === 'success' && response.data.data && response.data.data.translatedText) {
+          return response.data.data.translatedText;
+        } else {
+          console.error('Invalid API Response:', response.data);
+          throw new Error('Invalid API response format');
+        }
+      } catch (error) {
+        throw error;
+      }
+    };
+
     try {
-      const params = {
-        q: text,
-        target: targetLanguage,
-        source: 'en',
-      };
-
-      const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2?' + queryString.stringify(params);
-
-      const options = {
-        method: 'POST',
-        url,
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-          'Accept-Encoding': 'application/gzip',
-          'X-RapidAPI-Key': '38ea9d64acmshde920b4a986cd9cp145759jsnf3fe7545575e',
-          'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-        },
-      };
-
-      const response = await axios(options);
-      console.log('API Response:', response);
-      return response.data.data.translations[0].translatedText;
+      if (Array.isArray(text)) {
+        // Handle an array of texts for translation
+        return Promise.all(text.map(translateSingleText));
+      } else {
+        // Handle a single text for translation
+        return translateSingleText(text);
+      }
     } catch (error) {
       console.error('Translation error:', error);
       throw error;
@@ -48,7 +64,7 @@ const FlashcardScreen = ({ route }) => {
   // Find the selected category in the JSON data
   const selectedCategoryData = jsonData.categories.find(category => category.name === selectedCategory);
 
-  // Initialize flashcards state with translated words
+// Initialize flashcards state with translated words
   const [flashcards, setFlashcards] = useState([]);
 
   useEffect(() => {
@@ -57,14 +73,21 @@ const FlashcardScreen = ({ route }) => {
     console.log(selectedCategory);
     console.log("Selected Category Data:", selectedCategoryData);
 
-    if (selectedCategoryData) {
-      const translatedFlashcards = selectedCategoryData.words.map((word, index) => ({
-        id: index + 1, // Use a unique identifier (replace with a proper ID if needed)
-        question: word,
-        answer: translateText(word, selectedLanguage),
-      }));
-      setFlashcards(translatedFlashcards);
-    }
+    const translateFlashcards = async () => {
+      if (selectedCategoryData) {
+        const translatedFlashcards = await Promise.all(selectedCategoryData.words.map(async (word, index) => {
+          const translatedWord = await translateText(word, selectedLanguage);
+          return {
+            id: index + 1, // Use a unique identifier (replace with a proper ID if needed)
+            question: word,
+            answer: translatedWord,
+          };
+        }));
+        setFlashcards(translatedFlashcards);
+      }
+    };
+
+    translateFlashcards();
   }, [selectedCategoryData, selectedLanguage]);
 
   return (
